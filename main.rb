58a5330f21c26b5e73779481b03de9269068247a
7minutech <elijohnson1107@gmail.com>
2024-08-10 14:22:11 -0500

    Added debugging print
    Loops through HashMap buckets and prints them

--- main.rb
+++ main.rb
@@ -1,4 +1,10 @@
 require_relative "lib/hash_map"
 animals = HashMap.new
 animals.set("henry", "is a cool guy")
-p animals.buckets
+animals.buckets.each do |key_value_pair|
+  if key_value_pair.nil?
+    puts " [nil]"
+  else
+    puts key_value_pair
+  end
+end
