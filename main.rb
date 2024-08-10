ca699d2e31dced0815d467c0b9815360a59dcaec
7minutech <elijohnson1107@gmail.com>
2024-08-10 17:20:16 -0500

    Added more testing to main
    Tests to see if more than one node can work in
    the same bucket and overwrite the values to the
    keys

--- main.rb
+++ main.rb
@@ -1,9 +1,20 @@
 require_relative "lib/hash_map"
 humans = HashMap.new
 humans.set("henry", "is a cool guy")
-humans.buckets.each do |key_value_pair|
-  next if key_value_pair.nil?
-
-  puts key_value_pair
+humans.set("henry", "is a bad guy")
+humans.set("mia", "is a name")
+humans.set("mouse", "is an animal")
+humans.set("mouse", "is not an animal")
+def tester(name, hash)
+  puts name
+  puts hash.hash(name)
 end
-p humans.get("henry")
+
+def print_hash(hash_map)
+  hash_map.buckets.each do |key_value_pair|
+    next if key_value_pair.nil?
+
+    puts key_value_pair
+  end
+end
+print_hash(humans)
