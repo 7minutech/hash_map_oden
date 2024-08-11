917178b690c71d9b0c6901cf1b3044a3ce607145
7minutech <elijohnson1107@gmail.com>
2024-08-11 15:03:42 -0500

    Removed testing from main

--- main.rb
+++ main.rb
@@ -12,17 +12,5 @@ test.set("ice cream", "white")
 test.set("jacket", "blue")
 test.set("kite", "pink")
 test.set("lion", "golden")
-def tester(name, hash)
-  puts name
-  puts hash.hash(name)
-end
 
-def display_hash(hash_map)
-  hash_map.buckets.each do |key_value_pair|
-    print "nil" if key_value_pair.nil?
-    puts key_value_pair
-  end
-  puts "----------------------------"
-end
-
-test.entries
+puts test.values
