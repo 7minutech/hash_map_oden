becb30a7dffb5b7deb8168e851c75fbf5e2c637b
7minutech <elijohnson1107@gmail.com>
2024-08-11 14:25:18 -0500

    Removed tests

--- main.rb
+++ main.rb
@@ -1,20 +1,28 @@
 require_relative "lib/hash_map"
-humans = HashMap.new
-humans.set("henry", "is a cool guy")
-humans.set("henry", "is a bad guy")
-humans.set("mia", "is a name")
-humans.set("mouse", "is an animal")
-humans.set("mouse", "is not an animal")
+test = HashMap.new
+test.set("apple", "red")
+test.set("banana", "yellow")
+test.set("carrot", "orange")
+test.set("dog", "brown")
+test.set("elephant", "gray")
+test.set("frog", "green")
+test.set("grape", "purple")
+test.set("hat", "black")
+test.set("ice cream", "white")
+test.set("jacket", "blue")
+test.set("kite", "pink")
+test.set("lion", "golden")
 def tester(name, hash)
   puts name
   puts hash.hash(name)
 end
 
-def print_hash(hash_map)
+def display_hash(hash_map)
   hash_map.buckets.each do |key_value_pair|
-    next if key_value_pair.nil?
-
+    print "nil" if key_value_pair.nil?
     puts key_value_pair
   end
+  puts "----------------------------"
 end
-print_hash(humans)
+
+test.entries
